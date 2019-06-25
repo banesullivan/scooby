5ed028efeded7d9bd19ec9367b2af421d415f0d1
Bane Sullivan <banesullivan@gmail.com>
2019-06-25 17:03:19 -0600

    Only list packages without __version__ in knowledge.py

--- scooby/knowledge.py
+++ scooby/knowledge.py
@@ -1,13 +1,10 @@
 """
 A module for storing the path to a version string/variable for many of the
-common packages in the Python stack.
+common packages in the Python stack that have something other than a
+``__version__`` attribute.
 """
 
 VERSION_ATTRIBUTES = {
-    'numpy' : '__version__',
-    'scipy' : '__version__',
-    'pyvista' : '__version__',
     'vtk' : 'VTK_VERSION',
     'vtkmodules.all' : 'VTK_VERSION',
-
 }
