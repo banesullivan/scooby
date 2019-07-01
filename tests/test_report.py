3fb561e7e16d4aea6620f71ec38f9d302e9765fa
prisae <mail@werthmuller.org>
2019-07-01 22:34:29 +0200

    Add two more

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,14 +1,16 @@
 import re
+import bs4
 import scooby
-from bs4 import BeautifulSoup
 
 
 def test_plain_html_are_the_same():
-    # Get a `Report`-instance.
-    out = scooby.Report()
+    # Get a `Report`-instance, containing packages with don't exist, and such
+    # without a version number; also, containing all three lists.
+    out = scooby.Report(
+            [re, 'no_version'], ['does_not_exist', bs4], [scooby, 'numpy'])
 
     # Get html and plain text version of the repr.
-    text_html = BeautifulSoup(out._repr_html_()).get_text()
+    text_html = bs4.BeautifulSoup(out._repr_html_()).get_text()
     text_plain = out.__repr__()
 
     # Extract only letters and numbers.
@@ -17,3 +19,17 @@ def test_plain_html_are_the_same():
 
     # Ensure they are the same.
     assert text_html == text_plain
+
+
+def test_no_version():
+    # Get a `Report`-instance.
+    out = scooby.Report(['no_version'])
+
+    assert scooby.report.VERSION_NOT_FOUND in out.__repr__()
+
+
+def test_does_not_exist():
+    # Get a `Report`-instance.
+    out = scooby.Report(['does_not_exist'])
+
+    assert scooby.report.MODULE_NOT_FOUND in out.__repr__()
