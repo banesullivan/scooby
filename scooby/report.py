0a045875b1e5ba098a4c6abb458b47262f62eeaf
Bane Sullivan <banesullivan@gmail.com>
2019-12-23 16:15:44 -0500

    Add extra meta info pairs (#39)
    * Add extra meta info pairs
    
    * Move extra meta to end
    
    * Reformat extra_meta to be consistent with other entries
    
    * Fix tests

--- scooby/report.py
+++ scooby/report.py
@@ -156,9 +156,12 @@ class Report(PlatformInfo, PythonInfo):
     sort : bool, optional
         Sort the packages when the report is shown
 
+    extra_meta : tuple(str, str)
+        Additional two component pairs of meta information to display
+
     """
     def __init__(self, additional=None, core=None, optional=None, ncol=3,
-                 text_width=80, sort=False,):
+                 text_width=80, sort=False, extra_meta=None,):
 
         # Set default optional packages to investigate
         if optional is None:
@@ -168,6 +171,19 @@ class Report(PlatformInfo, PythonInfo):
                             optional=optional, sort=sort)
         self.ncol = int(ncol)
         self.text_width = int(text_width)
+
+        if extra_meta is not None:
+            if not isinstance(extra_meta, (list, tuple)):
+                raise TypeError("`extra_meta` must be a list/tuple of key-value pairs.")
+            if len(extra_meta) == 2 and isinstance(extra_meta[0], str):
+                extra_meta = [extra_meta]
+            for meta in extra_meta:
+                if not isinstance(meta, (list, tuple)) or len(meta) != 2:
+                    raise TypeError("Each chunk of meta info must have two values.")
+        else:
+            extra_meta = []
+        self._extra_meta = extra_meta
+
 
     def __repr__(self):
         """Plain-text version information."""
@@ -192,6 +208,8 @@ class Report(PlatformInfo, PythonInfo):
         if TOTAL_RAM:
             text += '{:>18}'.format(self.total_ram)+' : RAM\n'
         text += '{:>18}'.format(self.python_environment)+' : Environment\n'
+        for meta in self._extra_meta:
+            text += '{:>18}'.format(meta[1])+' : {}\n'.format(meta[0])
 
         # ########## Python details ############
         text += '\n'
@@ -267,6 +285,8 @@ class Report(PlatformInfo, PythonInfo):
             html, i = cols(html, self.total_ram, 'RAM', self.ncol, i)
         html, i = cols(
                 html, self.python_environment, 'Environment', self.ncol, i)
+        for meta in self._extra_meta:
+            html, i = cols(html, meta[1], meta[0], self.ncol, i)
         # Finish row
         html += "  </tr>\n"
 
